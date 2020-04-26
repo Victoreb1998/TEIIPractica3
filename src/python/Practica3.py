4c4337fe22b872a380d7cfb77a0b5f1a20e0c3ab
Victoreb <victor.espinb@um.es>
2020-04-26 18:36:19 +0200

    Bucle de metodos

--- src/python/Practica3.py
+++ src/python/Practica3.py
@@ -10,6 +10,9 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 numeros = []
+tiemposSet=[]
+tiemposDic=[]
+numerosProcesar=2000
 # Código main:
 def main():   
     # Control de argumentos de línea de comandos:
@@ -21,12 +24,20 @@ ficheroSalida = sys.argv[2]
 with open(ficheroEntrada,'r') as reader:
     for line in reader:
         numeros.append(int(line))
-t0 = time.time_ns()
-sinRepeticiones1=list(set(numeros))
-t1 = time.time_ns()
-t2 = time.time_ns()
-sinRepeticiones2=dict.fromkeys(numeros).keys()
-t3 = time.time_ns()
+sinRepeticiones1=[]
+sinRepeticiones2=[]
+while(numerosProcesar <= 200000):
+    aux=numeros[0:numerosProcesar]
+    t0 = time.time_ns()
+    sinRepeticiones1=list(set(aux))
+    t1 = time.time_ns()
+    t2 = time.time_ns()
+    sinRepeticiones2=dict.fromkeys(aux).keys()
+    t3 = time.time_ns()
+    tiemposSet.append(t1-t0)
+    tiemposDic.append(t3-t2)
+    numerosProcesar+=2000
+
 fichero = open(ficheroSalida,'w')
 for numero in sinRepeticiones1:
         fichero.write(str(numero) + '\n')
