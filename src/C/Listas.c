c42cffac1a6b66cd45bd6678d83b9a55fe11fe7c
Victoreb <victor.espinb@um.es>
2020-04-29 18:21:08 +0200

    lista.c arreglado

--- src/C/Listas.c
+++ src/C/Listas.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "mult.h"
+#include "Listas.h"
 
 // Función que multiplica todos los elementos de un vector «vin» de tamaño
 // «size» por un factor de escala «scale», dejando el resultado en el vector
 // «vout».
 int* Listas(int * lista, const int size)
 {
-    int i;
+
 	int aux = size;
-    for(i=0;i<aux;i++) {
-        for(j=i+1; j < aux; j++){
+    for(int i=0;i<aux;i++) {
+        for(int j=i+1; j < aux; j++){
 			if(lista[i] == lista[j]){
-				for(k=j; k<aux; k++){
+				for(int k=j; k<aux; k++){
 					lista[k]=lista[k+1];
 				}
 				aux--;
